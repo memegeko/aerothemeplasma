3afd6b6b250b3432af445b8e1bfd6c0c72a56107
meme_geko <lucasingmarwiersma1@gmail.com>
2026-08-05 20:17:06 +0200

    Add the Aero7 light glass Plasma style

--- plasma/shells/io.gitgud.wackyideas.desktop/contents/main.js
+++ plasma/shells/io.gitgud.wackyideas.desktop/contents/main.js
@@ -4,6 +4,8 @@ loadTemplate("io.gitgud.wackyideas.taskbar")
 var desktopsArray = desktopsForActivity(currentActivity());
 for( var j = 0; j < desktopsArray.length; j++) {
     desktopsArray[j].wallpaperPlugin = 'org.kde.image';
+    desktopsArray[j].currentConfigGroup = ["Wallpaper", "org.kde.image", "General"];
+    desktopsArray[j].writeConfig("Image", "file:///usr/share/wallpapers/Aero7ShellDefault/contents/images/1672x941.png");
+    desktopsArray[j].writeConfig("PreviewImage", "file:///usr/share/wallpapers/Aero7ShellDefault/contents/images/1672x941.png");
     //var clock = desktopsArray[j].addWidget("org.kde.plasma.analogclock");
 }
-
